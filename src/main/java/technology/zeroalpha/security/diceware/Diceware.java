13019efd6eaab95e56aca6225c396bca529b7c45
Chris Rowe <chris@zeroalpha.technology>
2016-10-26 16:02:39 +0100

    Added main Java class

---
+++ src/main/java/technology/zeroalpha/security/diceware/Diceware.java
@@ -0,0 +1,7 @@
+package technology.zeroalpha.security.diceware;
+
+/**
+ * Service used to transform results of a series of dice rolls into a word, based on a pre-defined dictionary.
+ */
+public class Diceware {
+}
